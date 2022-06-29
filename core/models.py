156ffe9bf445d804474bcf793fa739786b1fb321
Cyberneticus777 <marosleo1990@gmail.com>
2022-06-29 12:01:51 -0300

    Commit Inicial

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
@@ -12,6 +14,7 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
